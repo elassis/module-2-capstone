81aebcd4f87f1600b88f315bd2969ec09c6f5953
enmanuel <enmanuellassis@gmail.com>
2021-08-17 11:42:24 -0400

    index.js merge

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-
-import './style.css';
-
-const component = () => {
-  console.log('works')
-}
-
-component();
-=======
 import './style.css'; 
 
 const component = () => {
@@ -15,4 +5,3 @@ const component = () => {
 } 
 
 component(); 
->>>>>>> 3fbf81d1e4250407d9ac4b59f9b06d52a426308f
