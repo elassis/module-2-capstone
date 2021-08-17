3fbf81d1e4250407d9ac4b59f9b06d52a426308f
JbirdL86 <jgonzalez.wj@gmail.com>
2021-08-17 09:02:43 -0600

    Ignoring json packages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import _ from 'lodash';
-import './style.css';
+import './style.css'; 
 
-function component() {
-  const element = document.createElement('div');
+const component = () => {
+   console.log('works') 
+} 
 
- // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+component(); 
